748df06cf6c139c5cd4d674d827018920889d364
Joseph Bloom <jsphbloom@gmail.com>
2024-11-17 10:00:12 -0700

    Conditionals practice

--- comparisons.rb
+++ comparisons.rb
@@ -23,25 +23,25 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
-
+puts "Is number_teachers less than number_students?", number_teachers < number_students
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
-
+puts "Is number_teachers equal to number_students?", number_teachers == number_students
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
-
+puts "Is number_teachers less than number_students?", number_teachers != number_students
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
-
+puts "Is number_students greater than or equal to 20?", number_students >= 20
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
-
+puts "Is number_students greater than or equal to 21?", number_students >= 21
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
-
+puts "Is number_students less than or equal to 20?", number_students <= 20
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
-
+puts "Is number_students less than or equal to 21?", number_students >= 21
 
 #-------------------
 # PART 2: Articulating what you are doing
@@ -54,21 +54,24 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
-
+# this will display "true", as it uses a comparison operator boolean to compare two integers using the "less than" operator.
 books = 3
 puts 4 < books
 # YOU DO: Explain.
-
+# "books" is defined as the number 3. a comparison operator boolean is displayed comparing the number 4 to "books", which is defined as 3.
+# it will return "false".
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
-
+# "friends" is defined as the integer 6, "siblings" is defined as the integer 2. A comparison operator boolean is displayed, reading "false".
+# This is because when the comparison operator is ran, it identifies the words as the integers we defined them as.
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
-
+# "attendees" is defined as 9, "meals" is defined as 8. 'puts' displays the result of the comparison operator used, "!=", which means not equal.
+# the displayed result is "true".
 
 #-------------------
 # PART 3: Logical Operators
@@ -90,16 +93,18 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
-
+puts loves_to_play && loves_treats
+#yes and yes
 # Determine if the dog loves to play and loves the dog park
-
-
+puts loves_to_play && loves_dog_park
+#yes and no
 # Determine if the dog loves to play or loves the dog park
-
-
+puts loves_to_play || loves_dog_park
+#loves to play, not the dog park
 # Determine if the dog loves to play and is a puppy
 # Assume that a dog is a puppy if it is less than 2 years old.
-
+puts loves_to_play && age
 # What did your final line of code evaluate to? Why do you think that is? Explain.
 # ANSWER:
+#The final line output the boolean "true" as the dog loves to play. Age outputted as 1. The dog is a puppy and loves to play.
+
--- self_eval.md
+++ self_eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 1. When is it useful to use a `conditional` in your code?
+# it is useful when you want to have different output based on your input.
 1. If you could do this assignment over, what would you do differently?
+# I'd start simpler, I overcomplicated things for a while there.
 1. What about your thinking, learning, or work today brought you the most satisfaction? Why?
+# It was really satisfying when I thought I was overwhelmed and the work was too difficult, but after rereading the question, it simplified and I walked myself to the solution without looking for aid.
